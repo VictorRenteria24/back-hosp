789c29143399a8f05a0f6b48517e0e457f8c0ab7
VictorRenteria24 <espumitarmz@gmail.com>
2025-03-07 12:34:12 -0600

    Preparar backend 3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6000;
 
 const sequelize = require('./config/database');
 const User = require('./models/User');
