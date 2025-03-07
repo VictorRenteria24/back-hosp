db916d3432c40c27fa7430384d7261f062f5793b
VictorRenteria24 <espumitarmz@gmail.com>
2025-03-07 12:54:50 -0600

    Preparar backend 4

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('hospital', 'victor24', 'Merlin24$', {
-  host: 'localhost',
-  dialect: 'mysql',  // Especificamos que usamos MySQL
-  port: 3306,       // Puerto por defecto de MySQL en XAMPP
+const sequelize = new Sequelize(process.env.mysql://root:QEisbHUGFlkPcgUAAZiAuYHTWaiIIbWK@mysql.railway.internal:3306/railway, {
+  dialect: 'mysql',
 });
 
 module.exports = sequelize;
