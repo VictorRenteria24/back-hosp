55d3aa2ae78d659ee905c3a33358980adea774c0
VictorRenteria24 <espumitarmz@gmail.com>
2025-03-10 11:24:21 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('hospital', 'victor24', 'Merlin24$', {
-  host: 'localhost',
-  dialect: 'mysql',  // Especificamos que usamos MySQL
-  port: 3306,       // Puerto por defecto de MySQL en XAMPP
-});
+const sequelize = new Sequelize(
+  process.env.railway, // Nombre de la BD
+  process.env.victor24,     // Usuario
+  process.env.Merlin24$, // Contraseña
+  {
+    host: process.env.mysql.railway.internal, // Host de Railway
+    dialect: 'mysql',
+    port: process.env.MYSQL_PORT || 3306,
+    logging: false
+  }
+);
 
 module.exports = sequelize;
-
